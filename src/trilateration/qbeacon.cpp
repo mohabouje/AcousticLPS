3ef04ae05626f45fef806371ff1eceed313fb4b0
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-04-22 21:38:48 +0200

    Implementing QBeacon to use Qt's classes like QPoint, QUuid or QString for the different parameter.

--- src/trilateration/qbeacon.cpp
+++ src/trilateration/qbeacon.cpp
@@ -1,6 +1,39 @@
 #include "qbeacon.h"
 
-QBeacon::QBeacon(QObject *parent) : QObject(parent)
+QBeacon::QBeacon(QObject *parent) :
+    QObject(parent),
+    Beacon()
 {
 
 }
+
+QUuid QBeacon::universalUniqueIdentifier() const {
+    const QString tmp = QString::fromStdString(Beacon::getId());
+    return QUuid(tmp);
+}
+
+QPoint QBeacon::position() const {
+    return QPoint(Beacon::getX(), Beacon::getY());
+}
+
+int QBeacon::identifier() const {
+    return Beacon::getLocationId();
+}
+
+void QBeacon::setPosition(const QPoint &point) {
+    Beacon::setX(point.x());
+    Beacon::setY(point.y());
+    emit onPositionChanged(point);
+}
+
+void QBeacon::setUniversalUniqueIdentifier(const QUuid &uuid) {
+    const std::string tmp = uuid.toString().toStdString();
+    Beacon::setBeaconId(tmp);
+    emit onUniversallyUniqueIdentifierChanged(uuid);
+}
+
+void QBeacon::setIdentifier(int id) {
+    Beacon::setLocationId(id);
+    emit onIdentifierChanged(id);
+}
+
--- src/trilateration/qbeacon.h
+++ src/trilateration/qbeacon.h
@@ -2,16 +2,25 @@
 #define QBEACON_H
 
 #include <QObject>
+#include <QPoint>
+#include <QUuid>
+#include <beacon.h>
 
-class QBeacon : public QObject
-{
+class QBeacon : public QObject, Beacon {
     Q_OBJECT
 public:
     explicit QBeacon(QObject *parent = 0);
-
-signals:
-
+    QUuid universalUniqueIdentifier() const;
+    QPoint position() const;
+    int identifier() const;
 public slots:
+    void setPosition(const QPoint& point);
+    void setUniversalUniqueIdentifier(const QUuid& uuid);
+    void setIdentifier(int id);
+signals:
+    void onUniversallyUniqueIdentifierChanged(const QUuid& uuid) const;
+    void onIdentifierChanged(int id) const;
+    void onPositionChanged(const QPoint& point) const;
 };
 
 #endif // QBEACON_H
