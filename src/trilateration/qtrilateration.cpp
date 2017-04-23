1dc62e0b76e62fc65687c95d5639f10098149ad5
Mohammed Boujemaoui <mohabouje@gmail.com>
2017-04-23 13:34:05 +0200

    First run of Armadillo with success.

--- src/trilateration/qtrilateration.cpp
+++ src/trilateration/qtrilateration.cpp
@@ -1,13 +1,8 @@
 #include "qtrilateration.h"
-#include <functional>
-#include <iostream>
-#include <algorithm>
-#include <cctype>
-
+#include <armadillo>
+using namespace arma;
 #include <QDebug>
-
-QTrilateration::QTrilateration() :
-    Trilateration()
+QTrilateration::QTrilateration()
 {
 
 }
@@ -38,6 +33,27 @@ QTrilateration::Error QTrilateration::calculatePosition(QTrilateration::Algorith
 
 
 QTrilateration::Error QTrilateration::solveLinearLeastSquares() const {
+    mat A(5, 5, fill::randu);
+    double x = A(1,2);
+
+    mat B = A + A;
+    mat C = A * B;
+    mat D = A % B;
+
+    cx_mat X(A,B);
+
+    B.zeros();
+    B.set_size(10,10);
+    B.ones(5,6);
+
+    B.print("B:");
+
+    mat::fixed<5,6> F;
+
+    double aux_mem[24];
+    mat H(&aux_mem[0], 4, 6, false);  // use auxiliary memory
+
+
     return NoError;
 }
 QTrilateration::Error QTrilateration::solveSingularValueDecomposition() const {
